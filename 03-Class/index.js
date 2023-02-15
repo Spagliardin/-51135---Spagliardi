5d9805269f6bd1930f5cfab635ffe3763de934d6
nspagliardi <nspagliardi@eiwa.ag>
2023-02-15 12:10:09 -0300

    fix tab size

--- 03-Class/index.js
+++ 03-Class/index.js
@@ -1,49 +1,49 @@
 class ProductManager {
 
-    constructor(products = []){
-        this.products = products
-    }
+	constructor(products = []){
+			this.products = products
+	}
 
-    static id = 1
+	static id = 1
 
-    // uid(){
-    //     return String(
-    //         Date.now().toString(32) + Math.random().toString(16)
-    //     ).replace(/\./g, '')
-    // }
+	// uid(){
+	//     return String(
+	//         Date.now().toString(32) + Math.random().toString(16)
+	//     ).replace(/\./g, '')
+	// }
 
-    addProduct(title, description, price, thumbnail, code, stock){
-        const isIdemCode = this.products.length ?
-                         this.products.filter(product => product.code === code).length > 0 : 
-                         false
+	addProduct(title, description, price, thumbnail, code, stock){
+		const isIdemCode = this.products.length ?
+											 this.products.filter(product => product.code === code).length > 0 : 
+											 false
 
-        // const id = this.uid()
-        
-        if (!isIdemCode) {
-            this.products = [...this.products, {
-                title,
-                description,
-                price,
-                thumbnail,
-                code,
-                stock,
-                id: ProductManager.id
-            }]
-            ProductManager.id++
-        } else {
-            throw new Error('No two codes can be the same')
-        }
-    }
+		// const id = this.uid()
+		
+		if (!isIdemCode) {
+			this.products = [...this.products, {
+				title,
+				description,
+				price,
+				thumbnail,
+				code,
+				stock,
+				id: ProductManager.id
+			}]
+			ProductManager.id++
+		} else {
+				throw new Error('No two codes can be the same')
+		}
+	}
 
-    getProducts(){
-        console.log(this.products);
-        return this.products
-    }
+	getProducts(){
+		console.log(this.products);
+		return this.products
+	}
 
-    getProductByID(id){
-        let productByID = this.products.filter(product => product.id === id)
-        productByID.length ? console.log(productByID) : console.error('Not Found')
-    }
+	getProductByID(id){
+		let productByID = this.products.filter(product => product.id === id)
+		productByID.length ? console.log(productByID) : console.error('Not Found')
+	}
 }
 
 // let prueba = new ProductManager()

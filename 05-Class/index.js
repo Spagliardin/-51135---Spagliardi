de61ec88b44fe9e976393ed3fae1ce41c9af2354
nspagliardi <nspagliardi@eiwa.ag>
2023-02-27 05:00:48 -0300

    refactor to indent

--- 05-Class/index.js
+++ 05-Class/index.js
@@ -2,113 +2,113 @@ import { writeFile, readFile } from "node:fs/promises";
 
 class ProductManager {
 
-	constructor(products = [], path = './'){
-		this.products = products
-		this.path = path
-	}
+  constructor(products = [], path = './') {
+    this.products = products
+    this.path = path
+  }
 
-	uid(){
-		return String(
-				Date.now().toString(32) + Math.random().toString(16)
-		).replace(/\./g, '')
-	}
+  uid() {
+    return String(
+      Date.now().toString(32) + Math.random().toString(16)
+    ).replace(/\./g, '')
+  }
 
-	async getFile(){
+  async getFile() {
     try {
 
       const url = `${this.path}products.json`
       const productsFromFile = await readFile(url)
       return productsFromFile
-      
-    } catch (error) {
-      return null 
-    }
-	}
 
-	async addProduct(title, description, price, thumbnail, code, stock){
+    } catch (error) {
+      return null
+    }
+  }
+
+  async addProduct(title, description, price, thumbnail, code, stock) {
 
     this.products = JSON.parse(await this.getFile()) ?? []
 
-		const isIdemCode = this.products ?
-										   this.products.filter(product => product.code === code).length > 0 : 
-										   false
-    
-    
-		
-		if (!isIdemCode) {
-			this.products = [...this.products, {
-				title,
-				description,
-				price,
-				thumbnail,
-				code,
-				stock,
-				id: this.uid()
-			}]
+    const isIdemCode = this.products ?
+      this.products.filter(product => product.code === code).length > 0 :
+      false
 
-		await writeFile(`${this.path}products.json`, JSON.stringify(this.products))
 
-		} else {
-			throw new Error('No two codes can be the same')
-		}
-	}
 
-	async getProducts(){
-		let parsedProducts;
-		try {
+    if (!isIdemCode) {
+      this.products = [...this.products, {
+        title,
+        description,
+        price,
+        thumbnail,
+        code,
+        stock,
+        id: this.uid()
+      }]
 
-			parsedProducts = JSON.parse(await this.getFile()) ?? []
+      await writeFile(`${this.path}products.json`, JSON.stringify(this.products))
+
+    } else {
+      throw new Error('No two codes can be the same')
+    }
+  }
+
+  async getProducts() {
+    let parsedProducts;
+    try {
+
+      parsedProducts = JSON.parse(await this.getFile()) ?? []
       console.log(parsedProducts);
-			return parsedProducts
+      return parsedProducts
 
-		} catch (error) {
+    } catch (error) {
 
-			if (!parsedProducts) {
-				console.error('Empty File', []);
-			} else {
-				console.error(error);
-			}
-			
-		}
-	}
+      if (!parsedProducts) {
+        console.error('Empty File', []);
+      } else {
+        console.error(error);
+      }
 
-	async getProductByID(id){
-		const products = JSON.parse(await this.getFile())
-		const productByID = products.filter(product => product.id === id)
-		productByID.length ? console.log(productByID, 'Soy getProductByID') : console.error('Product not found')
-		return productByID[0]
-	}
+    }
+  }
 
-	async updateProduct(id, product){
+  async getProductByID(id) {
+    const products = JSON.parse(await this.getFile())
+    const productByID = products.filter(product => product.id === id)
+    productByID.length ? console.log(productByID, 'Soy getProductByID') : console.error('Product not found')
+    return productByID[0]
+  }
+
+  async updateProduct(id, product) {
 
     if (product.id) {
       console.error('the id cannot be changed')
       return
     }
 
-		const productbyID = await this.getProductByID(id)
-		const updateProduct = Object.keys(productbyID).reduce((obj, key) => {
-			if (product[key]) {
-				obj[key] = product[key]
-			} else {
-				obj[key] = productbyID[key]
-			}
-			return obj
-		}, {})
-		this.products = await this.deleteProduct(productbyID.id)
-		await writeFile(`${this.path}products.json`, JSON.stringify([...this.products, updateProduct]))
-	}
+    const productbyID = await this.getProductByID(id)
+    const updateProduct = Object.keys(productbyID).reduce((obj, key) => {
+      if (product[key]) {
+        obj[key] = product[key]
+      } else {
+        obj[key] = productbyID[key]
+      }
+      return obj
+    }, {})
+    this.products = await this.deleteProduct(productbyID.id)
+    await writeFile(`${this.path}products.json`, JSON.stringify([...this.products, updateProduct]))
+  }
 
-	async deleteProduct(id){
-	  const productbyID = await this.getProductByID(id)
+  async deleteProduct(id) {
+    const productbyID = await this.getProductByID(id)
     const products = await this.getProducts()
     const productsWithoutEliminated = products.filter(products => products.id !== productbyID.id)
     await writeFile(`${this.path}products.json`, JSON.stringify(productsWithoutEliminated))
     return productsWithoutEliminated
-	}
+  }
 }
 
-let test = new ProductManager()
+const test = new ProductManager()
 // await test.getProducts()
 // await test.addProduct('producto prueba', 'Este es un producto prueba', 200, 'Sin Imagen', 'abc123', 25)
 // await test.getProducts()
